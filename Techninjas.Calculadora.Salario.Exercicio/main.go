d9afa23aa9072ff8983de9f3afb573cd90fb811e
ThiagoAvelino1990 <thiagoavelinoalves@hotmail.com>
2022-08-08 23:22:45 -0300

    Importanto funções do pacote

--- Techninjas.Calculadora.Salario.Exercicio/main.go
+++ Techninjas.Calculadora.Salario.Exercicio/main.go
@@ -6,18 +6,19 @@ import (
 )
 
 func main() {
-	salarioBruto := trabalhador.SalarioBruto
-	//nomeTrabalhador := trabalhador.Nome
+
+	fmt.Println("Favor infromar o nome: ")
+	fmt.Scanln(&trabalhador.Nome)
 
 	fmt.Println("Favor informar o salario bruto: ")
 	//Função Scanf utilizada para leitura dos dados informados pelo usuário
-	fmt.Scanf("%f", &salarioBruto)
+	fmt.Scanln(&trabalhador.SalarioBruto)
 
 	//O comando %.2f indica a formatação de string para float com duas casas decimais após a vírgula
 	//O comando \n indica pular linha, uma vez que está sento utilizando o fmt.Printf para impresão dos dados na tela
-	fmt.Printf("Valor do desconto de IR: %.2f\n", trabalhador.CalcularIR(salarioBruto))
-	fmt.Printf("Valor do desconto de INSS: %.2f\n", trabalhador.CalcularINSS(salarioBruto))
+	fmt.Printf("Valor do desconto de IR: %.2f\n", trabalhador.CalcularIR(trabalhador.SalarioBruto))
+	fmt.Printf("Valor do desconto de INSS: %.2f\n", trabalhador.CalcularINSS(trabalhador.SalarioBruto))
 
-	fmt.Printf("Valor total do salario: %.2f\n", trabalhador.CalculaSalarioLiquido(salarioBruto))
+	fmt.Print("O Nome do trabalhador é: ", trabalhador.Nome, " Valor total do salario: ", trabalhador.CalculaSalarioLiquido(trabalhador.SalarioBruto))
 
 }
